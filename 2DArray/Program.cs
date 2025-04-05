71e4c220b6e72620d4d5ec267551f99c3e0e2b2c
Camille  Dickens <camilledickens@Mac.attlocal.net>
2025-04-05 18:44:37 -0400

    Add option to display 2D array as numbers or X/O based on parity

--- 2DArray/Program.cs
+++ 2DArray/Program.cs
@@ -16,14 +16,20 @@ namespace MyApp
             const int HIGH_NUMBER = 100;
             const string arrayFigure1 = "X";
             const string arrayFigure2 = "O";
-            int randomNumber = rng.Next(LOW_NUMBER, HIGH_NUMBER);
+            const int symbolVariable = 1;
+            const int numberVariable = 2;
             bool assign = false;
             int randomRows = rng.Next(LOW_NUMBER, HIGH_NUMBER);
             int randomColumns = rng.Next(LOW_NUMBER, HIGH_NUMBER);
 
 
-            Console.WriteLine($"A random row will generate: {randomRows}");
+            Console.WriteLine($"Enter {symbolVariable} for X and O");
+            Console.WriteLine($"Please enter {numberVariable} for numbers");
+            int userChoice = int.Parse(Console.ReadLine());
 
+
+
+            Console.WriteLine($"A random row will generate: {randomRows}");
             Console.WriteLine($"A random column will generate: {randomColumns}");
 
             int[,] numbers = new int[randomRows, randomColumns];
@@ -39,22 +45,44 @@ namespace MyApp
 
             Console.WriteLine("Here is your 2D array:");
 
-            for (int i = 0; i < randomRows; i++)
+            if (userChoice == symbolVariable)
             {
-                for (int j = 0; j < randomColumns; j++)
+
+                for (int i = 0; i < randomRows; i++)
                 {
-                    if (numbers[i, j] % 2 == 0)
+                    for (int j = 0; j < randomColumns; j++)
                     {
-                        Console.Write(arrayFigure2.PadLeft(4)); 
+                        if (numbers[i, j] % 2 == 0)
+                        {
+                            Console.Write(arrayFigure2.PadLeft(4));
+                        }
+                        else
+                        {
+                            Console.Write(arrayFigure1.PadLeft(4));
+                        }
                     }
-                    else
+
+                    Console.WriteLine();
+                }
+            }
+
+            else if (userChoice == numberVariable)
+            {
+                for (int i = 0; i < randomRows; i++)
+                {
+                    for (int j = 0; j < randomColumns; j++)
                     {
-                        Console.Write(arrayFigure1.PadLeft(4)); 
+                        Console.Write(numbers[i, j].ToString().PadLeft(4));
                     }
+                    Console.WriteLine();
                 }
 
-                Console.WriteLine();
             }
+            else
+            {
+                Console.WriteLine("Invalid input. Please restart and enter 1 or 2.");
+            }
+
         }
-    }
+    } 
 }
